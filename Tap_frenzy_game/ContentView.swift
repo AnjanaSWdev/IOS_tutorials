4f17cdfe1d01b4a0e2c9d156f4f833addea4a35b
AnjanaSWdev <anjanagp05@gmail.com>
2026-06-11 21:35:15 +0530

    Initial Commit

---
+++ Tap_frenzy_game/ContentView.swift
@@ -0,0 +1,115 @@
+//
+//  ContentView.swift
+//  Tap_frenzy_game
+//
+//  Created by TEST on 2026-06-10.
+//
+
+import SwiftUI
+internal import Combine
+
+struct ContentView: View {
+    
+    @State var countDownTimer = 30
+    @State var timerRunning = false
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State var score = 0
+    
+    @State private var showGameOver = false
+    
+    private func resetGame() {
+        
+            showGameOver = false
+            countDownTimer = 30
+            score = 0
+            
+        }
+    
+    var body: some View {
+        VStack {
+           
+            Text("Tap Frenzy")
+                .frame(alignment: .top)
+                .font(.largeTitle)
+                .bold()
+                .padding(20)
+            
+            Text("Score : \(score)")
+                .font(.system(size: 25))
+            Spacer()
+            
+            Button(action: {
+                if(countDownTimer > 0 && timerRunning == true){
+                    score += 1
+                }
+                
+            }) {
+                Text("TAP")
+                    .font(.headline)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .frame(width: 200, height: 200)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                
+            }
+            .contentShape(Circle())
+            .padding(60)
+            
+            
+            Button(action: {
+                if(countDownTimer == 0){
+                    countDownTimer = 30
+                    timerRunning = true
+                }else{
+                    timerRunning = true
+                }
+                
+            }) {
+                Text("Start")
+                    .bold()
+                    .font(.system(size: 22))
+                    .foregroundColor(.white)
+                    .frame(width: 250, height: 60)
+                    .background(Color.red)
+                    .clipShape(Rectangle())
+            }
+            Spacer()
+            
+            
+            VStack{
+                Text("\(countDownTimer)").onReceive(timer){ _ in
+                    if countDownTimer > 0 && timerRunning{
+                        countDownTimer -= 1
+                    }else if countDownTimer == 0{
+                        timerRunning = false
+                        showGameOver = true
+                    }
+                }
+                .font(.system(size: 45))
+                .alert("Game Over", isPresented: $showGameOver) {
+                    Button("Play Again") {
+                        
+                        resetGame()
+                        
+                    }
+                } message: {
+                    Text("Final Score: \(score) points")
+                }
+                
+            }
+            
+        }
+    }
+    
+        
+
+        
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ Tap_frenzy_game/Tap_frenzy_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tap_frenzy_gameApp.swift
+//  Tap_frenzy_game
+//
+//  Created by TEST on 2026-06-10.
+//
+
+import SwiftUI
+
+@main
+struct Tap_frenzy_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
